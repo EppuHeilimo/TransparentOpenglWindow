e3c02cd3d5f8edf1a55ad3629ddb79bec4fa8460
Eppu Heilimo <eppu.heilimo@yahoo.com>
2016-12-01 14:53:18 +0200

    sdaweqsa

--- Unnamed engine/Main.cpp
+++ Unnamed engine/Main.cpp
@@ -5,6 +5,7 @@
 #include <GL/wglew.h>
 #include <dwmapi.h>
 #include <cassert>
+#include <cmath>
 #pragma comment(lib, "glew32.lib")
 
 int main(int argc, char ** argv)
@@ -23,7 +24,7 @@ int main(int argc, char ** argv)
 	SDL_Window *window;
 	window = SDL_CreateWindow("asd",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		1024, 800, SDL_WINDOW_OPENGL);
+		1680, 1024, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(window);
 
 
@@ -33,7 +34,8 @@ int main(int argc, char ** argv)
 	}
 	glEnable(GL_ALPHA_TEST);
 	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBlendFuncSeparate(GL_ONE, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDisable(GL_CULL_FACE);
 	bool closed = false;
 	SDL_Event e;
 
@@ -53,36 +55,50 @@ int main(int argc, char ** argv)
 
 	//updatelayeredwindow <<--- CHECK OUT http://stackoverflow.com/questions/18383681/setlayeredwindowattributes-to-make-a-window-transparent-is-only-working-part-of
 	//CHECK OUT http://www.nuonsoft.com/blog/2009/05/27/how-to-use-updatelayeredwindow/
-
+	float c1 = 0.15435435f;
+	float c2 = 0.5543543f;
+	float c3 = 0.7543543f;
+	float t = 0;
 	DWM_BLURBEHIND bb = { 0 };
 	bb.dwFlags = DWM_BB_ENABLE | DWM_BB_BLURREGION;;
 	bb.fEnable = true;
 	bb.hRgnBlur = CreateRectRgn(0, 0, -1, -1);
 	HRESULT qwe;
+	glMatrixMode(GL_PROJECTION);
 	qwe = DwmEnableBlurBehindWindow(handle, &bb);
 	if (SUCCEEDED(qwe))
 	{
 		while (!closed)
 		{
+			
+			t+=0.01f;
 			while (SDL_PollEvent(&e)) {
 				switch (e.type) {
 				case SDL_QUIT:
 					closed = true;
 				}
 			}
-			glClearColor(0, 0, 0, 0.5);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			glColor3f(0, 1, 1);
-			glBegin(GL_TRIANGLES);                              // Drawing Using Triangles
-			glColor3f(1.0f, 0.0f, 0.0f);                      // Set The Color To Red
-			glVertex3f(0.0f, 1.0f, 0.0f);                  // Top
-			glColor3f(0.0f, 1.0f, 0.0f);                      // Set The Color To Green
-			glVertex3f(-1.0f, -1.0f, 0.0f);                  // Bottom Left
-			glColor3f(0.0f, 0.0f, 1.0f);                      // Set The Color To Blue
-			glVertex3f(1.0f, -1.0f, 0.0f);                  // Bottom Right
-			glEnd();
+			
+			glLoadIdentity();
+			glClearColor(0, 0, 0, 0);
+			//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glRotatef(t * 5, 0, 0, 1);
+			glBegin(GL_TRIANGLE_STRIP);
 
+			for (int i = 0; i < 10; i++)
+			{
+				float shit = sin(i * i * sqrtf(float(i))) / 200;				                           // Drawing Using Triangles
+				glColor4f(abs(sin(c1 * t)), abs(sin(c2 * t)), abs(sin(c3 * t)), shit);                      // Set The Color To Red
+				glVertex3f(sin(c1 * t) * shit * shit, sin(c2 * t * shit), 0.0f);						// Top
+				glColor4f(abs(sin(c2 * t)), abs(sin(c3 * t)), abs(sin(c1 * t)), sin(shit));                      // Set The Color To Green
+				glVertex3f(sin(c3 * t * shit + shit), sin(c1 * t * shit + shit), 0.0f);                  // Bottom Left
+				glColor4f(abs(sin(c3 * t)), abs(sin(c1 * t)), abs(sin(c2 * t)), cos(shit * sin(shit)));                      // Set The Color To Blue
+				glVertex3f(sin(c2 * t * shit), sin(c3 * t / shit), 0.0f);                  // Bottom Right
+				
+			}
+			glEnd();
 			SDL_GL_SwapWindow(window);
+			
 		}
 	}
 	SDL_GL_DeleteContext(window);
