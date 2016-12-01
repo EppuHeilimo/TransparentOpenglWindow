7e27f5e73a41e345d50d21a4c32bfe206bd2d297
Eppu Heilimo <eppu.heilimo@yahoo.com>
2016-12-01 14:07:50 +0200

    Fix shit

--- Unnamed engine/Main.cpp
+++ Unnamed engine/Main.cpp
@@ -4,6 +4,7 @@
 #include <GL/glew.h>
 #include <GL/wglew.h>
 #include <dwmapi.h>
+#include <cassert>
 #pragma comment(lib, "glew32.lib")
 
 int main(int argc, char ** argv)
@@ -25,10 +26,14 @@ int main(int argc, char ** argv)
 		1024, 800, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(window);
 
+
 	GLenum status = glewInit();
 	if (status != GLEW_OK) {
 		std::cerr << "Glew failed to initialize!\n";
 	}
+	glEnable(GL_ALPHA_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	bool closed = false;
 	SDL_Event e;
 
@@ -36,33 +41,50 @@ int main(int argc, char ** argv)
 	SDL_VERSION(&sysinfo.version);
 	SDL_GetWindowWMInfo(window, &sysinfo);
 	HWND handle = sysinfo.info.win.window;
-	
+	int asd = 0;
+
 
 	//Remove all cluster from window (title bar and borders)
 	SetWindowLong(handle, GWL_STYLE, ::GetWindowLong(handle, GWL_STYLE) & ~(WS_BORDER | WS_DLGFRAME | WS_THICKFRAME));
 	SetWindowLong(handle, GWL_EXSTYLE, ::GetWindowLong(handle, GWL_EXSTYLE) & ~WS_EX_DLGMODALFRAME);
-
-	//make window layered, set "color" as transparency
-	COLORREF color = RGB(0xFF, 0xFF, 0x00);
-	SetWindowLong(handle, GWL_EXSTYLE, GetWindowLong(handle, GWL_EXSTYLE) | WS_EX_LAYERED);
-	SetLayeredWindowAttributes(handle, color, 50, LWA_ALPHA | LWA_COLORKEY);
+	SetWindowLong(handle, GWL_STYLE, GetWindowLong(handle, GWL_STYLE) | WS_POPUP &~WS_OVERLAPPEDWINDOW);
+	//SetWindowLong(handle, GWL_EXSTYLE, GetWindowLong(handle, GWL_EXSTYLE) | WS_EX_LAYERED);
+	
 
 	//updatelayeredwindow <<--- CHECK OUT http://stackoverflow.com/questions/18383681/setlayeredwindowattributes-to-make-a-window-transparent-is-only-working-part-of
 	//CHECK OUT http://www.nuonsoft.com/blog/2009/05/27/how-to-use-updatelayeredwindow/
-	while(!closed)
-	{
-		while (SDL_PollEvent(&e)) {
-			switch (e.type) {
-			case SDL_QUIT:
-				closed = true;
-			}
-		}
-		glClearColor(255, 0, 0, 255);
-		glClear(GL_COLOR_BUFFER_BIT);
-		SDL_GL_SwapWindow(window);
-		
-	}
 
+	DWM_BLURBEHIND bb = { 0 };
+	bb.dwFlags = DWM_BB_ENABLE | DWM_BB_BLURREGION;;
+	bb.fEnable = true;
+	bb.hRgnBlur = CreateRectRgn(0, 0, -1, -1);
+	HRESULT qwe;
+	qwe = DwmEnableBlurBehindWindow(handle, &bb);
+	if (SUCCEEDED(qwe))
+	{
+		while (!closed)
+		{
+			while (SDL_PollEvent(&e)) {
+				switch (e.type) {
+				case SDL_QUIT:
+					closed = true;
+				}
+			}
+			glClearColor(0, 0, 0, 0.5);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glColor3f(0, 1, 1);
+			glBegin(GL_TRIANGLES);                              // Drawing Using Triangles
+			glColor3f(1.0f, 0.0f, 0.0f);                      // Set The Color To Red
+			glVertex3f(0.0f, 1.0f, 0.0f);                  // Top
+			glColor3f(0.0f, 1.0f, 0.0f);                      // Set The Color To Green
+			glVertex3f(-1.0f, -1.0f, 0.0f);                  // Bottom Left
+			glColor3f(0.0f, 0.0f, 1.0f);                      // Set The Color To Blue
+			glVertex3f(1.0f, -1.0f, 0.0f);                  // Bottom Right
+			glEnd();
+
+			SDL_GL_SwapWindow(window);
+		}
+	}
 	SDL_GL_DeleteContext(window);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
